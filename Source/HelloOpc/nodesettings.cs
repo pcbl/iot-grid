3d13783fd65a39192778b30e6a6ff49742e3c2ac
lnfe <lnfe@DEPC008350>
2020-04-08 13:20:34 +0200

    added new class and appsettings to get the node informations easier (not working yet)

---
+++ Source/HelloOpc/nodesettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloOpc
+{
+    public class nodesettings
+    {
+        public string Id { get; set; }
+        public string NsId { get; set; }
+    }
+}
