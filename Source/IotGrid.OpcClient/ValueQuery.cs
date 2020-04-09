10b90c24019559825f421b9f75619f4488aa43ef
lnfe <lnfe@DEPC008350>
2020-04-09 10:09:37 +0200

    Scafolding from Projects

---
+++ Source/IotGrid.OpcClient/ValueQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IotGrid.OpcClient
+{
+    public class ValueQuery
+    {
+        public object CurrentValue(string nodeId)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Source/IotGrid.WebApi/Controllers/OpcUaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IotGrid.OpcClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IotGrid.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OpcUaController : ControllerBase
+    {
+
+        private readonly ILogger<OpcUaController> _logger;
+
+        public OpcUaController(ILogger<OpcUaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet,Route("current/{nodeId}")]
+        public ActionResult<string> Current(string nodeId)
+        {
+            ValueQuery query = new ValueQuery();
+            return Ok(query.CurrentValue(nodeId));
+        }
+    }
+}
